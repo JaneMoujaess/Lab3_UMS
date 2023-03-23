4bcedfe72a1717f02556f40e735890a0249c19ae
Jane <jmoujaess123@hotmail.com>
2023-03-23 13:01:18 +0200

    Add additional files

---
+++ Lab3.Domain/Models/BranchTenant.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab3.Domain.Models;
+
+public partial class BranchTenant
+{
+    public string Id { get; set; } = null!;
+
+    public string Location { get; set; } = null!;
+}
