06b515e77f9496b8305b4346a9716f8bbd364cbb
Jane <jmoujaess123@hotmail.com>
2023-03-30 20:41:24 +0300

    Commit NotificationDto

---
+++ Lab3.Application/DTOs/NotificationDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Lab3.Application.DTOs;
+
+public class NotificationDto
+{
+    public long TeacherId { set; get; }
+    public long StudentId { set; get; }
+    public long CourseId { set; get; }
+}
